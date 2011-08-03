035e070b40496bd01b827e7825ac9b346a5f5ce6
U-KnownSubset\Nathan <Nathan@KnownSubset.me>
2011-08-02 23:18:29 -0500

    Changed data structure of the dates in the records to be more forgiving.  Add ability to sort records.

--- historical_data_fetcher.erl
+++ historical_data_fetcher.erl
@@ -12,7 +12,7 @@ fetch(Name) ->
 fetch(Name, Range) ->
     {ok, Conn} = mongo:connect (getHost()),
     {ok, Prices} = mongo:do(safe, master, Conn, test, fun () ->
-        StockHistory = mongo:rest(mongo:find(stock, {name, bson:utf8(Name)},{'_id', 0, close, 1}, 0, -1*Range)),
+        StockHistory = mongo:rest(mongo:find(stock, {'query',{name, bson:utf8(Name)}, 'orderby', {date,-1}},{'_id', 0, close, 1}, 0, -1*Range)),
         lists:map (fun (Closing) -> bson:at (close, Closing) end, StockHistory) end),
     mongo:disconnect (Conn),
 	Prices.
@@ -29,10 +29,16 @@ getHost() ->
 insert(_, _, []) ->  null;
 insert(Name, Conn, [H|T])->
     Elements = string:tokens(H,?TOKENS),
+    DateParts = string:tokens(lists:nth(1,Elements),"/"),
+    Year = list_to_number(lists:nth(3,DateParts)),
+    Month = list_to_number(lists:nth(1,DateParts)),
+    Day = list_to_number(lists:nth(2,DateParts)),
     mongo:do(safe, master, Conn, test, fun () ->
         mongo:insert(stock, {
             name, bson:utf8(Name),
-            date, bson:utf8(lists:nth(1,Elements)),
+            dayOfYear, calendar:date_to_gregorian_days(Year, Month, Day),
+            secondOfDay, calendar:time_to_seconds({16,0,0}),
+            date, {year, Year, month, Month, day, Day, hour, 16, minute, 0, second, 0},
             open, list_to_number(lists:nth(2,Elements)),
             high, list_to_number(lists:nth(3,Elements)),
             low, list_to_number(lists:nth(4,Elements)),
