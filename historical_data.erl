9db55a434b3c1a3009a0afcaa136765228de1a58
U-KnownSubset\Nathan <Nathan@KnownSubset.me>
2011-08-08 21:36:39 -0500

    Can now fetch items based upon days or minutes preference

--- historical_data.erl
+++ historical_data.erl
@@ -1,40 +1,49 @@
 -module(historical_data).
--export([fetch/1,fetch/2, import/2, write/6]).
+-export([fetch/1,fetch/3, import/2, write/7]).
 -define(DATABASE_URL, "databaseURL").
 -define(DATABASE_PORT, "databasePort").
 -define(FILE_TOKENS, ",\n").
 -define(DATE_TOKENS, "/").
 
 fetch(Name) ->
-	fetch(Name, 60).
+	fetch(Name, 60, minutes).
 
 %Todo - need to pull out the values that are eod closes, or by minutes out of the whole collection
-fetch(Name, Range) ->
+fetch(Name, Range, minutes) ->
+    {{Year,Month,Day},{Hour,Minutes,Seconds}} = erlang:localtime(),
+    MarketClosingTime = convertDateToSeconds(Year-1,Month,Day,Hour,Minutes,Seconds),
+    fetchPricesAfterMarketClosingTime(Name, Range, MarketClosingTime);
+
+fetch(Name, Range, days) ->
+    MarketClosingTime = convertDateToSeconds(1970, 1, 1, 16, 0, 0),
+    fetchPricesAfterMarketClosingTime(Name, Range, MarketClosingTime).
+
+fetchPricesAfterMarketClosingTime(Name, Range, MarketClosingTime) ->
     {ok, Conn} = mongo:connect (getHost()),
     {ok, Prices} = mongo:do(safe, master, Conn, test, fun () ->
-        StockHistory = mongo:rest(mongo:find(stock, {'query',{name, bson:utf8(Name)}, 'orderby', {time,-1}},{'_id', 0, close, 1}, 0, -1*Range)),
+        StockHistory = mongo:rest(mongo:find(stock, {'$query',{name, bson:utf8(Name), time, {'$gte', MarketClosingTime }}, '$orderby', {timestamp,1}},{'_id', 0, close, 1}, 0, -1*Range)),
         lists:map (fun (Closing) -> bson:at (close, Closing) end, StockHistory) end),
     mongo:disconnect (Conn),
 	Prices.
 
-write(Name, High, Low, Close, Volume, Time) ->
+write(Name, High, Low, Close, Volume, Timestamp, Time) ->
     {ok, Conn} = mongo:connect(getHost()),
-    mongo:do(safe, master, Conn, test, fun () ->
-        mongo:delete(stock,  {name, bson:utf8(Name)}) end),
-    insert(Conn, Name, High, Low, Close, Volume, Time),
+    insert(Conn, Name, High, Low, Close, Volume, [Timestamp|Time]),
     mongo:disconnect (Conn).
 
 import(Name, FileName) ->
     Lines = file_reader:read(FileName),
     {ok, Conn} = mongo:connect(getHost()),
-    insert(Name, Conn, Lines),
+    mongo:do(safe, master, Conn, test, fun () ->
+        mongo:delete(stock,  {name, bson:utf8(Name)}) end),
+    insertRecords(Name, Conn, Lines),
     mongo:disconnect (Conn).
 
 getHost() ->
     {properties:getSingleProperty(?DATABASE_URL), list_to_number(properties:getSingleProperty(?DATABASE_PORT))}.
 
-insert(_, _, []) ->  null;
-insert(Name, Conn, [H|T])->
+insertRecords(_, _, []) ->  null;
+insertRecords(Name, Conn, [H|T])->
     Elements = string:tokens(H,?FILE_TOKENS),
     Time =  convertDateStringToSeconds(lists:nth(1,Elements)),
     High = list_to_number(lists:nth(3,Elements)),
@@ -42,11 +51,11 @@ insert(Name, Conn, [H|T])->
     Close = list_to_number(lists:nth(5,Elements)),
     Volume = list_to_integer(lists:nth(6,Elements)),
     insert(Conn, Name, High, Low, Close, Volume, Time),
-    insert(Name,Conn, T).
+    insertRecords(Name,Conn, T).
 
-insert(Conn, Name, High, Low, Close, Volume, Time)->
+insert(Conn, Name, High, Low, Close, Volume, [Timestamp | Time])->
     mongo:do(safe, master, Conn, test, fun () ->
-        mongo:insert(stock,  {name, bson:utf8(Name), time, Time, high, High,
+        mongo:insert(stock,  {name, bson:utf8(Name),timestamp, Timestamp, time, Time, high, High,
                               low, Low, close, Close, volume, Volume}) end).
 
 convertDateStringToSeconds(DateString) ->
@@ -54,7 +63,7 @@ convertDateStringToSeconds(DateString) ->
     Year = list_to_number(lists:nth(3,DateParts)),
     Month = list_to_number(lists:nth(1,DateParts)),
     Day = list_to_number(lists:nth(2,DateParts)),
-    convertDateToSeconds(Year, Month, Day, 16, 0, 0).
+    [convertDateToSeconds(Year, Month, Day, 16, 0, 0) | convertDateToSeconds(1970, 1, 1, 16, 0, 0) ].
 
 convertDateToSeconds(Year, Month, Day, Hour, Minutes, Seconds) ->
     bson:secs_to_unixtime(86400 * calendar:date_to_gregorian_days(Year-1970, Month, Day) + calendar:time_to_seconds({Hour,Minutes,Seconds})).
--- stock_watcher.erl
+++ stock_watcher.erl
@@ -18,7 +18,7 @@ watch(NAME, PERIOD, HIGHS, LOWS, CLOSES) ->
 			%timer:sleep(12000),
             %                                                     lists:foldl(fun(X, Sum) ->  math:pow(X - Mean,2) + Sum end, 0, Closes)
             file_writer:write(NAME, lists:foldl(fun(X, Text) ->  Text++ ","++ float_to_list(X) end, "", [High,Low,Close,IchimokuCloud,Macd,BollingerBands])),
-            historical_data:write(NAME, High, Low, Close, Volume, erlang:now()),
+            historical_data:write(NAME, High, Low, Close, Volume, erlang:now(), erlang:time()),
             %
 			watch(NAME, PERIOD, Highs, Lows, Closes);
     terminate ->
